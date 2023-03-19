0d2be1a0f285acf942385908be17f36c8f7ccbd5
Arsentii Hrybok <gribok.work@gmail.com>
2023-03-19 18:37:19 +0100

    chore(refactor): 1672;

--- src/1672. Richest Customer Wealth.ts
+++ src/1672. Richest Customer Wealth.ts
@@ -1,33 +1,39 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from '@jest/globals';
 
 function maximumWealth(accounts: number[][]): number {
-  return accounts.reduce((acc, cur) => {
-    const currentUserWealth = cur.reduce((acc, cur) => acc + cur, 0);
-    return currentUserWealth > acc ? currentUserWealth : acc;
-  }, 0);
+    return accounts.reduce((acc, cur) => {
+        const currentUserWealth = cur.reduce((acc, cur) => acc + cur, 0);
+
+        return currentUserWealth > acc ? currentUserWealth : acc;
+    }, 0);
 }
 
-describe("1672. Richest Customer Wealth", () => {
-  test(`Given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.`, () => {
-    expect(
-      maximumWealth([
-        [1, 2, 3],
-        [3, 2, 1],
-      ])
-    ).toEqual(6);
-    expect(
-      maximumWealth([
-        [1, 5],
-        [7, 3],
-        [3, 5],
-      ])
-    ).toEqual(10);
-    expect(
-      maximumWealth([
-        [2, 8, 7],
-        [7, 1, 3],
-        [1, 9, 5],
-      ])
-    ).toEqual(17);
-  });
+describe('1672. Richest Customer Wealth. Given an m x n integer grid accounts where accounts[i][j] is the amount of money the customer [I] has in the bank [J]. Return the wealth that the richest customer has.', () => {
+    test.each([
+        {
+            accounts: [
+                [1, 2, 3],
+                [3, 2, 1],
+            ],
+            expected: 6,
+        },
+        {
+            accounts: [
+                [1, 5],
+                [7, 3],
+                [3, 5],
+            ],
+            expected: 10,
+        },
+        {
+            accounts: [
+                [2, 8, 7],
+                [7, 1, 3],
+                [1, 9, 5],
+            ],
+            expected: 17,
+        },
+    ])('%j', ({ accounts, expected }) => {
+        expect(maximumWealth(accounts)).toEqual(expected);
+    });
 });
