e32a6f8ed17905f15d1b9fc97e044f14a4dbfd0e
Arsentii Hrybok <gribok.work@gmail.com>
2023-03-19 18:34:29 +0100

    chore(refactor): 1480;

--- src/1480. Running Sum of 1d Array.ts
+++ src/1480. Running Sum of 1d Array.ts
@@ -8,10 +8,16 @@ function runningSum(nums: number[]): number[] {
     }, []);
 }
 
-describe('1480. Running sum of 1d Array', () => {
-    test(`Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum of nums`, () => {
-        expect(runningSum([1, 2, 3, 4])).toEqual([1, 3, 6, 10]);
-        expect(runningSum([1, 1, 1, 1, 1])).toEqual([1, 2, 3, 4, 5]);
-        expect(runningSum([3, 1, 2, 10, 1])).toEqual([3, 4, 6, 16, 17]);
+describe('1480. Running sum of 1d Array. Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum of nums', () => {
+    test.each([
+        { nums: [1], expected: [1] },
+        { nums: [1, 2], expected: [1, 3] },
+        { nums: [1, 2, 3, 4], expected: [1, 3, 6, 10] },
+        { nums: [1, 1, 1, 1, 1], expected: [1, 2, 3, 4, 5] },
+        { nums: [3, 1, 2, 10, 1], expected: [3, 4, 6, 16, 17] },
+        { nums: [3, 1, 5, 10, 1], expected: [3, 4, 9, 19, 20] },
+        { nums: [3, 1, 100, 14123, 1], expected: [3, 4, 104, 14227, 14228] },
+    ])(`$nums => $expected`, ({ nums, expected }) => {
+        expect(runningSum(nums)).toEqual(expected);
     });
 });
